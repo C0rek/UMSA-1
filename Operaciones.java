90111b3e9d2ac78e7cd14d696f23829a21cbcdb5
Tomas Rodriguez Zoni <tomasrodriguezzoni2412@gmail.com>
2024-05-16 16:30:11 -0300

    Ejercicios java 16/05
    Agrego ejercicios de la clase 16/5 al repositorio

---
+++ Operaciones.java
@@ -0,0 +1,20 @@
+package main;
+import java.util.Scanner;
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		int valor1 = scanner.nextInt();
+		int valor2 = scanner.nextInt();
+		
+		
+		scanner.close();
+		
+		
+
+	}
+
+}
